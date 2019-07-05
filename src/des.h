969285bb8dedc072d3d33b18aef291440026bbd9
cyyzero <cyyzero16@qq.com>
2019-07-05 10:18:53 +0800

    Update DES  to encrypt and decrypt  bytes

--- src/des.h
+++ src/des.h
@@ -1,7 +1,31 @@
 #ifndef DES_H
 #define DES_H
 
-void DES_Efun(const char input[8], char inKey[8], int output[64]);
-void DES_Dfun(const int input[64], char inKey[8], char output[8]);
+#include <iostream>
+#include <bitset>
+
+typedef std::bitset<64> Block;
+typedef std::bitset<56> Key;
+typedef std::bitset<48> Code;
+
+typedef std::bitset<32> HBlock;
+typedef std::bitset<28> HKey;
+typedef std::bitset<24> HCode;
+
+enum Method
+{
+    e,
+    d
+};
+
+int ip(const Block &block, HBlock &left, HBlock &right);
+int des_turn(HBlock &left, HBlock &right, const Code &subkey);
+int exchange(HBlock &left, HBlock &right);
+int rip(const HBlock &left, const HBlock &right, Block &block);
+Code getkey(const unsigned int n, const Block &bkey);
+int des(Block &block, Block &bkey, const Method method);
+
+std::string des_encrypt(std::string msg, Block& bkey);
+std::string des_decrypt(const std::string& msg, Block& bkey);
 
 #endif // DES_H
