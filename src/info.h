585f1f99c04d1d2e1c6ba761aa9eb103dc6d6311
cyyzero <cyyzero16@qq.com>
2019-07-05 20:46:46 +0800

    Add pay_info and order_info

---
+++ src/info.h
@@ -0,0 +1,18 @@
+#ifndef INFO_H
+#define INFO_H
+
+#include <cstdint>
+
+struct Payment_info
+{
+    uint64_t comm_id;
+    uint64_t money;
+};
+
+struct Order_info
+{
+    uint64_t account_id;
+    uint64_t money;
+};
+
+#endif // INFO_H
